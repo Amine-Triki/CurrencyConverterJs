dd7d21d7fe1997c483332b0773588304b1350834
Amine Triki <amine.triki15@gmail.com>
2026-04-13 14:24:59 +0100

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     
     <>
       <footer className="footer py-3 "style={{ backgroundColor: '#b388ff' }}  >
-        <p id="Rights" className="container text-center mb-0">2023 -- {`${year} © Amine Triki || All Rights Reserved`} ||<span> <Link to="/terms_of_service" className='text-dark'>Terms of Service</Link>  <Link to="/privacy_policy" className='text-dark' >Privacy Policy</Link></span> </p>
+        <p id="Rights" className="container text-center mb-0">2025 -- {`${year} © Amine Triki || All Rights Reserved`}  </p>
     </footer>
     </>
   )
